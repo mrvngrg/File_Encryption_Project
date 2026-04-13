d9bd6fb0eb2f6905336ae942413e478ac3f978e6
FredericWeyssow <frederic.weyssow@gmail.com>
2026-04-13 13:22:23 +0200

    Add server implementation

--- server.c
+++ server.c
@@ -5,33 +5,63 @@
 #include <stdbool.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define PORT 8080
+
+struct server {
+    int serverfd;
+    int new_socket;
+} server;
 
 struct sockaddr_in address;
 socklen_t addrlen = sizeof(address);
 
-void *connection_listener(int serverfd) {
+void *connection_listener() {
     while (true) {
-        listen(serverfd, 3);
-        int new_socket = accept(serverfd, (struct sockaddr*) &address, &addrlen);
+        listen(server.serverfd, 3);
+        printf("listen\n");
+        int new_socket = accept(server.serverfd, (struct sockaddr*) &address, &addrlen);
+        printf("test");
         if (new_socket == 0) {
-            printf("client is connected");
+            printf("client is connected\n");
+            server.new_socket = new_socket;
         }      
     }
 }
 
-void *receive() {
-    recv( sockfd, *buf, len, flags);
+void *message_listener() {
+    char buffer[1024] = { 0 };
+    while (true) {
+        read(server.new_socket, buffer, 1024 - 1);
+        printf("%s\n", buffer);
+    }
 }
 
+int main() {
 
-
-int main(int argc, char *argv) {
     int serverfd = socket(AF_INET, SOCK_STREAM, 0);
+    server.serverfd = serverfd;
     int opt = 1;
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
     bind(serverfd, (struct sockaddr*)&address, addrlen);
     //setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    pthread_t tids[1];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    for (int i = 0; i < 2; i++) {
+        pthread_create(&tids[i], &attr, connection_listener, NULL);
+    }
+
+    for (int i = 0; i < 2; i++) {
+        pthread_join(tids[i], NULL);
+    }
 
     
     //should have a queue that stores ID's
