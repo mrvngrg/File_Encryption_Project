e031df280718476582207d5e53bc1a046f339e5f
FredericWeyssow <frederic.weyssow@gmail.com>
2026-04-13 13:36:31 +0200

    Update server implementation

--- server.c
+++ server.c
@@ -17,53 +17,67 @@ struct server {
 struct sockaddr_in address;
 socklen_t addrlen = sizeof(address);
 
-void *connection_listener() {
+void *message_listener(void *arg) {
+    char buffer[1024];
     while (true) {
-        listen(server.serverfd, 3);
-        printf("listen\n");
-        int new_socket = accept(server.serverfd, (struct sockaddr*) &address, &addrlen);
-        printf("test");
-        if (new_socket == 0) {
-            printf("client is connected\n");
-            server.new_socket = new_socket;
-        }      
+        memset(buffer, 0, sizeof(buffer));
+        int n = read(server.new_socket, buffer, sizeof(buffer) - 1);
+        if (n <= 0) {
+            printf("client disconnected\n");
+            break;
+        }
+        printf("received: %s\n", buffer);
     }
+    return NULL;
 }
 
-void *message_listener() {
-    char buffer[1024] = { 0 };
+void *connection_listener(void *arg) {
     while (true) {
-        read(server.new_socket, buffer, 1024 - 1);
-        printf("%s\n", buffer);
+        printf("waiting for connection\n");
+        int new_socket = accept(server.serverfd, (struct sockaddr*) &address, &addrlen);
+        if (new_socket >= 0) {
+            printf("client connected\n");
+            server.new_socket = new_socket;
+
+            pthread_t msg_thread;
+            pthread_create(&msg_thread, NULL, message_listener, NULL);
+            pthread_detach(msg_thread);
+        } else {
+            perror("accept");
+        }
     }
+    return NULL;
 }
 
 int main() {
-
     int serverfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverfd < 0) {
+        perror("socket");
+        return 1;
+    }
     server.serverfd = serverfd;
+
     int opt = 1;
+    setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    bind(serverfd, (struct sockaddr*)&address, addrlen);
-    //setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    pthread_t tids[1];
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-
-    for (int i = 0; i < 2; i++) {
-        pthread_create(&tids[i], &attr, connection_listener, NULL);
+    if (bind(serverfd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind");
+        return 1;
     }
 
-    for (int i = 0; i < 2; i++) {
-        pthread_join(tids[i], NULL);
+    if (listen(serverfd, 3) < 0) {
+        perror("listen");
+        return 1;
     }
 
-    
-    //should have a queue that stores ID's
-    //should start thread that listen to new client and thread that listen to new informations about already connected sockets
-} 
+    pthread_t tid;
+    pthread_create(&tid, NULL, connection_listener, NULL);
+    pthread_join(tid, NULL);
+
+    close(serverfd);
+    return 0;
+}
