4874f0767c3882a6afa50572423982aac9ca063b
FredericWeyssow <frederic.weyssow@gmail.com>
2026-04-23 14:24:10 +0200

    Add a way to hide script and update todo

--- client.c
+++ client.c
@@ -62,7 +62,7 @@ int startclient() {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) { //127.0.0.1 //10.172.20.145
         printf("inet_pton failed\n");
         return -1;
     }
