656bf5191304f582fb736ce5585a1383f50761a0
Simon <simon.zeugin@stud.unibas.ch>
2026-06-01 16:29:31 +0200

    Add peek_and_dequeue so more prblems with later added files causeing race conditions (for now)

--- src/encryption/globals.c
+++ src/encryption/globals.c
@@ -3,9 +3,9 @@
 
 Queue queue;
 //const char *start_path = "/home/nicolas-berger/Documents/Safe/test_encryption";
-const char *start_path = "/home/drikson/University/os/test";
+// const char *start_path = "/home/drikson/University/os/test";
 //const char *start_path = "/home/vboxuser/test";
-//const char *start_path = "/home/simon/Desktop/test";
+const char *start_path = "/home/simon/Desktop/test";
 //const char *start_path = "/home";
 
 unsigned char key[16] = "qwertyuiopasdfgh";
--- src/encryption/thread.c
+++ src/encryption/thread.c
@@ -11,10 +11,17 @@
 
 void start_encrypt() {
     while (true) {
-        char *head = peek(&queue); //race condition maybe peek_and_dequeue.
-        if (head == NULL || strcmp(head, "END_ENCRYPT") == 0) break;
-        
-        char *data = dequeue(&queue);
+
+        char *data = peek_and_dequeue(&queue, "END_ENCRYPT");
+        if (data == NULL){
+            break;
+        }
+                
+        if (strstr(data, ".locked") != NULL) {
+            free(data);
+            continue;
+        }
+
         printf("TID: %lu: \n%s\n", (unsigned long)pthread_self(), data);
         char *filename = encrypt_file(data, key);
         enqueue(&queue, filename);
@@ -25,10 +32,17 @@ void start_encrypt() {
 
 void start_decrypt() {
     while (true) {
-        char *head = peek(&queue);
-        if (head == NULL || strcmp(head, "END_DECRYPT") == 0) break;
+        char *data = peek_and_dequeue(&queue, "END_DECRYPT");
 
-        char *data = dequeue(&queue);
+        if (data == NULL){
+            break;
+        }
+
+        if (strstr(data, ".locked") == NULL) {
+            free(data);
+            continue;
+        }
+
         printf("TID: %lu: \n%s\n", (unsigned long)pthread_self(), data);
         char *filename = decrypt_file(data, key);
         enqueue(&queue, filename);
